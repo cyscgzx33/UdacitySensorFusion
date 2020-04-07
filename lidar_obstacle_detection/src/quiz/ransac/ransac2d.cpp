92ccacd77b1dfe0bcfec3035fa793ff5a9fca331
Yusen Chen <chenyus@umich.edu>
2020-04-07 00:34:18 -0400

    feat: implemented the 2d line fitting RANSAC algorithm

--- lidar_obstacle_detection/src/quiz/ransac/ransac2d.cpp
+++ lidar_obstacle_detection/src/quiz/ransac/ransac2d.cpp
@@ -65,20 +65,47 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 {
 	std::unordered_set<int> inliersResult;
 	srand(time(NULL));
-	
-	// TODO: Fill in this function
 
+	// TODO (done): Fill in this function
 	// For max iterations 
+	for (int i = 0; i < maxIterations; i++)
+	{
+		// Randomly sample subset and fit line
+		int n = cloud->points.size();
+		int idx1, idx2;
+		idx1 = rand() % n;
+		idx2 = rand() % n;
+		while (idx1 == idx2) // make sure idx1 and idx2 are different
+			idx2 = rand() & n;
 
-	// Randomly sample subset and fit line
+		auto pt1 = cloud->points[idx1];
+		auto pt2 = cloud->points[idx2];
+		// Fit a line: Ax + By + C = 0
+		float A = pt1.y - pt2.y;
+		float B = pt2.x - pt1.x;
+		float C = pt1.x * pt2.y - pt2.x * pt1.y;
 
-	// Measure distance between every point and fitted line
-	// If distance is smaller than threshold count it as inlier
+		// Measure distance between every point and fitted line
+		// If distance is smaller than threshold count it as inlier
+		// According to the equation: (x*, y*) to line Ax + By + C has a distance:
+		// |Ax* + By* + C| / sqrt(A^2 + B^2)
+		float denom = sqrt(A * A + B * B);
+
+		// use a temp container to store inliers
+		std::unordered_set<int> inliers;
+		for (int index = 0; index < cloud->points.size(); index++)
+		{
+			auto pt = cloud->points[index];
+			float dist = fabs(A * pt.x + B * pt.y + C) / denom; // must use float version of abs, i.e., fabs()
+			if (dist < distanceTol)
+				inliers.insert(index);
+		}
+		if (inliers.size() > inliersResult.size())
+			inliersResult = inliers;
+	}
 
 	// Return indicies of inliers from fitted line with most inliers
-	
 	return inliersResult;
-
 }
 
 int main ()
@@ -91,8 +118,9 @@ int main ()
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
 	
 
-	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 0, 0);
+	// TODO (done): Change the max iteration and distance tolerance arguments for Ransac function
+	// rule of thumb: if having lower ratio of inliers, use a larger iteration number
+	std::unordered_set<int> inliers = Ransac(cloud, 64, 0.6);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
