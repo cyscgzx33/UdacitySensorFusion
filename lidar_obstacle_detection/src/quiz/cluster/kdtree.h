b9db992048bd7b1804b8841fd95f52b0be62d33c
Yusen Chen <chenyus@umich.edu>
2020-04-09 00:43:49 -0400

    feat: implemented kd tree insert, but there's some bug, need to fix soon

--- lidar_obstacle_detection/src/quiz/cluster/kdtree.h
+++ lidar_obstacle_detection/src/quiz/cluster/kdtree.h
@@ -3,17 +3,16 @@
 
 #include "../../render/render.h"
 
-
-// Structure to represent node of kd tree
+// Structure to represent node of KD-tree
 struct Node
 {
-	std::vector<float> point;
+	std::vector<float> point; // first one is x, second one is y
 	int id;
 	Node* left;
 	Node* right;
 
 	Node(std::vector<float> arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
+	:	point(arr), id(setId), left(nullptr), right(nullptr)
 	{}
 };
 
@@ -22,14 +21,35 @@ struct KdTree
 	Node* root;
 
 	KdTree()
-	: root(NULL)
+	: root(nullptr)
 	{}
 
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		// the function should create a new node and place correctly with in the root
+		Node* curr = root;
+		int depth = 0;
 
+		while (curr->point.size() > 0)
+		{
+			if (depth % 2 == 0) // check x split
+			{
+				if (point[0] > curr->point[0])
+					curr = curr->right;
+				else
+					curr = curr->left;
+			}
+			else // check y split
+			{
+				if (point[1] > curr->point[1])
+					curr = curr->right;
+				else
+					curr = curr->left;
+			}
+			depth++;
+		}
+		curr = new Node(point, id);
 	}
 
 	// return a list of point ids in the tree that are within distance of target
@@ -38,8 +58,6 @@ struct KdTree
 		std::vector<int> ids;
 		return ids;
 	}
-	
-
 };
 
 
